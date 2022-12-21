d99b715e4f4df014fb5ee28043b2423fb987b130
capmahin <mahinulhuque1996@gmail.com>
2022-12-22 04:14:42 +0600

    add Navbar and acor tags

---
+++ comps/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (  
+        <nav>
+            <div className="logo">
+                <h1>Ninja List</h1>
+            </div>
+            <a></a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
