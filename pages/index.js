e26e7bfb4ff9127f61207202378a643b6cf40602
capmahin <mahinulhuque1996@gmail.com>
2022-12-22 12:00:22 +0600

    Add Link in homepage

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Navbar from "../comps/Navbar"
 import Footer from "../comps/Footer"
-
+import Link from "next/link"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti ex saepe qui, aut consequatur quos nam doloribus tempora, quidem debitis quae blanditiis veritatis harum, facilis omnis. Provident, velit. Sequi, minus?</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti ex saepe qui, aut consequatur quos nam doloribus tempora, quidem debitis quae blanditiis veritatis harum, facilis omnis. Provident, velit. Sequi, minus?</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti ex saepe qui, aut consequatur quos nam doloribus tempora, quidem debitis quae blanditiis veritatis harum, facilis omnis. Provident, velit. Sequi, minus?</p>
-
+      <Link href="/ninjas">See Ninja Listing </Link>
       <Footer />
     </div>
   )
