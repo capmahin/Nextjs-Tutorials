9c6db90ed6133fb76f8deafaec00e736c86e92d6
capmahin <mahinulhuque1996@gmail.com>
2022-12-25 02:16:29 +0600

    destracturing

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,18 @@
 export const getStaticProps = async()=>{
 
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+   const data = await res.json()
+
+
+   return {
+    props:{
+        ninjas:data
+    }
+   }
 }
 
 
-const Ninjas = () => {
+const Ninjas = ({ninjas}) => {
     return ( <div>
         <h1>All Ninjas</h1>
     </div> );
