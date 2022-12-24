4cb7448ca6b77e8d71a14103891b0c19e13d4208
capmahin <mahinulhuque1996@gmail.com>
2022-12-25 02:23:26 +0600

    add ninjas propoerty

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -15,6 +15,15 @@ export const getStaticProps = async()=>{
 const Ninjas = ({ninjas}) => {
     return ( <div>
         <h1>All Ninjas</h1>
+        {
+            ninjas.map(ninja=>(
+                <div key={ninja.id}>
+                       <a >
+                        <h3>{ninja.name}</h3>
+                       </a>
+                </div>
+            ))
+        }
     </div> );
 }
  
