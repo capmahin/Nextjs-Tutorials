66c0eb7e637b2f97e1d4d04b9f1d0108fd4ee167
capmahin <mahinulhuque1996@gmail.com>
2022-12-25 02:06:49 +0600

    add head and title and meta name in about page

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About =()=>{
     return (
+        <>
+         <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"></meta>
+       </Head>
         <div>
             <h1>About</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quasi ratione quibusdam ipsum esse molestias repellendus qui, harum eum iusto corporis voluptas officiis tempore dolor debitis consequatur totam temporibus error.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque consequuntur praesentium recusandae molestiae fugit, nesciunt eius amet facere obcaecati aspernatur nobis, rerum quibusdam nostrum, quod neque sit vero adipisci?</p>
         </div>
+        </>
     );
 }
 
